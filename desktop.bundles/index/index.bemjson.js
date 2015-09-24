5758c64ae3d98a2904438aafd7fa94cc203c8118
Maximario <grevtsovm@yandex.ru>
2015-09-24 10:42:35 +0300

    Templ repo

---
+++ desktop.bundles/index/index.bemjson.js
@@ -0,0 +1,28 @@
+({
+    block: 'page',
+    title: 'Title of the page',
+    favicon: '/favicon.ico',
+    head: [
+        { elem: 'meta', attrs: { name: 'description', content: '' }},
+        { elem: 'css', url: 'index.min.css' }
+    ],
+    scripts: [{ elem: 'js', url: 'index.min.js' }],
+    content: [
+        {
+            block: 'head',
+            content: {
+                block: 'layout',
+                content: [
+                    {
+                        elem: 'left',
+                        content: 'left here'
+                    },
+                    {
+                        elem: 'right',
+                        content: 'right here'
+                    }
+                ]
+            }
+        }
+    ]
+})
