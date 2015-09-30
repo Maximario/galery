9bad9a6b2f94b81350140a0db227016b721bd61d
Maximario <grevtsovm@yandex.ru>
2015-09-30 22:24:23 +0300

    Good scroll

--- desktop.blocks/albumround/albumround.js
+++ desktop.blocks/albumround/albumround.js
@@ -8,9 +8,17 @@ provide(BEMDOM.decl(this.name, {
 				$albumBlock = albumBlock.domElem;
                 this.bindTo('mousewheel DOMMouseScroll', function(e) {
 					e.preventDefault();
-					console.log("Wheel " + e.originalEvent.wheelDelta);
-					console.log("Detail " + e.originalEvent.detail);
-					console.log("deltaY " + e.originalEvent.deltaY);
+					var leftint = parseInt($albumBlock.css('left').slice(0, -2));
+					rightlimit = parseInt(this.domElem.css('width').slice(0, -2)) - parseInt($albumBlock.css('width').slice(0, -2));
+					if ((leftint <= 0) || (leftint >= rightlimit)) {
+						var delta = (e.originalEvent.wheelDelta ? e.originalEvent.wheelDelta/120*70 : e.originalEvent.detail/(-3)*70) || false;
+						console.log("delta " + delta);
+						leftint += delta;
+						leftint = (leftint > 0 ? 0 : leftint);
+						leftint = (leftint < rightlimit ? rightlimit : leftint);
+						console.log("leftint " + leftint);
+						$albumBlock.animate({left: leftint}, 30);
+					}
 				});
             }
         }
